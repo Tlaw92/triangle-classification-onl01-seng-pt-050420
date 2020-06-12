0ee8aea431ee323cd1d7df42faf561708c5cb528
travis Lawson <travislawclimb@gmail.com>
2020-06-12 20:49:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+    if @s1 == 0 || @s2 == 0 || @s3 == 0 
+      raise ZeroLengthError
+    end   
+  end   
+  
+  
+  
+  class ZeroLengthError < StandardError
+    def message
+      "All three sides must be greater than 0"
+    end 
+  end   
+  
+  
 end
